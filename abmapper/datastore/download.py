72689ece34aea8a876e84a5347c99fd655484466
markbrough <mark.brough@publishwhatyoufund.org>
2014-11-24 20:18:38 +0100

    Generalise reporting org in URL

--- abmapper/datastore/download.py
+++ abmapper/datastore/download.py
@@ -7,9 +7,9 @@ from abmapper import app, db
 from abmapper import models
 from abmapper import projects
 
-URL = "http://iati-datastore.herokuapp.com/api/1/access/activity.xml?recipient-country=%s&reporting-org=CA-3&stream=True"
+URL = "http://iati-datastore.herokuapp.com/api/1/access/activity.xml?recipient-country=%s&reporting-org=%s&stream=True"
 
-def download_data(country):
+def download_data(country, reporting_org):
     def write_data(doc):
         LOCAL_DIR=app.config["DATA_STORAGE_DIR"]
         path = os.path.join(LOCAL_DIR, "iati_data_" + country + '.xml')
@@ -17,7 +17,7 @@ def download_data(country):
         with file(path, 'w') as localFile:
             localFile.write(iati_activities)
 
-    the_url = URL % (country)
+    the_url = URL % (country, reporting_org)
     print the_url
 
     xml_data = urllib2.urlopen(the_url, timeout=60).read()
