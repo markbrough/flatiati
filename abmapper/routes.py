3a7801156acc1f720660053fa10b835b79ed2ba2
markbrough <mark.brough@publishwhatyoufund.org>
2014-12-17 17:23:19 +0100

    Split into overview and projects pages

--- abmapper/routes.py
+++ abmapper/routes.py
@@ -20,18 +20,27 @@ def dashboard():
                           )
 
 @app.route("/<country_code>/")
-def home(country_code):
-    p = projects.projects(country_code)
+def country_home(country_code):
     stats = projects.country_project_stats(country_code)
     budget_stats = projects.budget_project_stats(country_code)
     country = projects.country(country_code)
     reporting_orgs = projects.reporting_org_activities(country_code)
     return render_template("country.html",
-                           projects=p,
                            country=country,
                            reporting_orgs=reporting_orgs,
                            stats=stats,
                            budget_stats=budget_stats,
+                           )
+
+@app.route("/<country_code>/activities/")
+def country_activities(country_code):
+    p = projects.projects(country_code)
+    country = projects.country(country_code)
+    reporting_orgs = projects.reporting_org_activities(country_code)
+    return render_template("country_activities.html",
+                           projects=p,
+                           country=country,
+                           reporting_orgs=reporting_orgs,
                            )
 
 @app.route("/<country_code>/activities/<iati_identifier>/")
