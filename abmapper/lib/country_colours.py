2faaf01a3d1218520bdd49ef385e534febc3d450
Mark Brough <mark.brough@gmail.com>
2015-03-15 18:01:36 +0100

    Handle exception where file doesn't exist

--- abmapper/lib/country_colours.py
+++ abmapper/lib/country_colours.py
@@ -5,8 +5,11 @@ from flask import escape
 def colours(country_code):
     out = {}
     filename=os.path.join(os.path.dirname(__file__), "colours_%s.csv") % escape(country_code)
-    with open(filename, "r") as csvfile:
-        csv = unicodecsv.DictReader(csvfile)
-        for row in csv:
-            out[row['name']] = row['colour']
-    return out
+    try:
+        with open(filename, "r") as csvfile:
+            csv = unicodecsv.DictReader(csvfile)
+            for row in csv:
+                out[row['name']] = row['colour']
+        return out
+    except IOError:
+        return None
