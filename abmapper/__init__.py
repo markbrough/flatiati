eb8f41277ab75a955bbf17eb29d7007d87ef19f9
Mark Brough <mark.brough@gmail.com>
2015-03-16 21:48:49 +0100

    Redirect back to same page, if possible

--- abmapper/__init__.py
+++ abmapper/__init__.py
@@ -3,6 +3,7 @@
 from flask import Flask, request, session, redirect, url_for, flash
 from flask.ext.babel import Babel
 from flask.ext.sqlalchemy import SQLAlchemy
+from urlparse import urlparse, urljoin
 import os
 
 app = Flask(__name__.split('.')[0])
@@ -22,12 +23,23 @@ def get_locale():
     # header the browser transmits. The best match wins.
     return request.accept_languages.best_match(LANGUAGES)
 
+def is_safe_url(target):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ('http', 'https') and \
+           ref_url.netloc == test_url.netloc
+
+def get_redirect_target():
+    if request.referrer and is_safe_url(request.referrer):
+        return request.referrer
+    return url_for("dashboard")
+
 @app.route("/lang/<lang>/")
 def set_lang(lang):
     print LANGUAGES
     if lang in LANGUAGES:
         session['lang'] = lang
-        return redirect(url_for('dashboard'))
+        return redirect(get_redirect_target())
     else:
         flash("Unrecognised language", "error")
-        return redirect(url_for('dashboard'))
+        return redirect(get_redirect_target())
