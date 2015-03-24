8e75d29ca767f78f19559a7cac4072887a54e725
Mark Brough <mark.brough@gmail.com>
2015-03-24 20:41:52 +0100

    Use sectors module instead

--- abmapper/views/projects.py
+++ abmapper/views/projects.py
@@ -2,12 +2,13 @@ from flask import render_template, request
 from abmapper import app
 from abmapper.lib import util
 from abmapper.query import projects as abprojects
+from abmapper.query import sectors as absectors
 
 @app.route("/<country_code>/activities/<iati_identifier>/")
 def activities(country_code, iati_identifier):
     a = abprojects.project(iati_identifier)
     country = abprojects.country(country_code)
-    sectors = abprojects.DAC_codes_cc_mappable()
+    sectors = absectors.DAC_codes_cc_mappable()
     return render_template("project.html",
                            activity=a,
                            sectors=sectors,
@@ -19,9 +20,9 @@ def activity_add_sector(country_code, iati_identifier):
     sector_code = request.form['sector_code']
     iati_identifier = request.form['iati_identifier']
     percentage = request.form['percentage']
-    sector_data = abprojects.sector(sector_code)
+    sector_data = absectors.sector(sector_code)
 
-    if abprojects.add_sector_to_project(sector_code, iati_identifier, percentage):
+    if absectors.add_sector_to_project(sector_code, iati_identifier, percentage):
         return util.jsonify({"cc": {
                                "sector": sector_data.sector, 
                                "function": sector_data.function, 
@@ -35,7 +36,7 @@ def activity_add_sector(country_code, iati_identifier):
 def activity_delete_sector(country_code, iati_identifier):
     sector_code = request.form['sector_code']
     iati_identifier = iati_identifier
-    result = abprojects.delete_sector_from_project(sector_code, iati_identifier)
+    result = absectors.delete_sector_from_project(sector_code, iati_identifier)
     if result:
         return util.jsonify({"success": result})
     return util.jsonify({"error": True})
@@ -44,6 +45,6 @@ def activity_delete_sector(country_code, iati_identifier):
 def activity_restore_sector(country_code, iati_identifier):
     sector_code = request.form['sector_code']
     iati_identifier = iati_identifier
-    if abprojects.restore_sector_to_project(sector_code, iati_identifier):
+    if absectors.restore_sector_to_project(sector_code, iati_identifier):
         return util.jsonify({"success": True})
     return util.jsonify({"error": True})
