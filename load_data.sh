9d7aefeba5e9624ac9c69874fbea0d71d76de3a4
Mark Brough <mark.brough@gmail.com>
2015-03-25 15:54:13 +0100

    Add batch script for loading in the data

---
+++ load_data.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+./abtool.py --setup
+./abtool.py --import --filename="data/senegal/xml/CA-3-SN.xml" --country-code="SN" --sample
+./abtool.py --update-projects --filename="data/senegal/xls/canada.xls" --country-code="SN" --reporting-org="CA-3"
+./abtool.py --import-budget --filename="data/senegal/senegal_CC_budget.csv" --country-code="SN" --budget-type="f"
+
+./abtool.py --import --filename="data/senegal/xml/undp-sn.xml" --country-code="SN"
+./abtool.py --update-projects --filename="data/senegal/xls/undp.xls" --country-code="SN" --reporting-org="41114"
+
+./abtool.py --import --filename="data/nepal/xml/dfid-np.xml" --country-code="NP"
+./abtool.py --update-projects --filename="data/nepal/xls/GB-1-NP-new.xls" --country-code="SN" --reporting-org="GB-1"
