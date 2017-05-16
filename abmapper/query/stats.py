b6d12c9a956f04b58ad841725ae9cebca3136344
Mark Brough <mark.brough@gmail.com>
2017-05-16 11:54:46 +0000

    Strip out a lot of functions from stats

--- abmapper/query/stats.py
+++ abmapper/query/stats.py
@@ -14,9 +14,9 @@ def budget_project_stats(country_code, budget_type):
     budgetcode.code, budgetcode.name
     FROM atransaction
     JOIN activity ON 
-        activity.iati_identifier=atransaction.activity_iati_identifier
+        activity.id=atransaction.activity_id
     JOIN sector ON 
-        activity.iati_identifier = sector.activity_iati_identifier
+        activity.id = sector.activity_id
     JOIN dacsector ON sector.code = dacsector.code
     JOIN commoncode ON dacsector.cc_id = commoncode.id
     JOIN ccbudgetcode ON commoncode.id = ccbudgetcode.cc_id
@@ -35,8 +35,8 @@ def budget_project_stats(country_code, budget_type):
     SELECT sum(atransaction.value*sector.percentage/100) AS sum_value,
     budgetcode.code, budgetcode.name
     FROM atransaction
-    JOIN activity ON activity.iati_identifier=atransaction.activity_iati_identifier
-    JOIN sector ON activity.iati_identifier = sector.activity_iati_identifier
+    JOIN activity ON activity.id=atransaction.activity_id
+    JOIN sector ON activity.id = sector.activity_id
     JOIN dacsector ON sector.code = dacsector.code
     JOIN commoncode ON dacsector.cc_id = commoncode.id
     JOIN ccbudgetcode ON commoncode.id = ccbudgetcode.cc_id
@@ -55,7 +55,7 @@ def budget_project_stats(country_code, budget_type):
     SELECT sum(atransaction.value) AS sum_value
     FROM atransaction
     JOIN activity ON 
-        activity.iati_identifier=atransaction.activity_iati_identifier
+        activity.id=atransaction.activity_id
     AND activity.recipient_country_code="%s"
     AND activity.aid_type_code IN ('C01', 'D01', 'D02')
     AND activity.status_code IN (2, 3)
@@ -72,11 +72,30 @@ def budget_project_stats(country_code, budget_type):
                 country_code
                 )).first()[0]
 
+    if not total:
+        return {
+            "total": 0,
+            "budgets": {
+                "-": {
+                    'code': '-',
+                    'name': "Unknown",
+                    'before': {
+                        'value': 0.00,
+                        'pct': 0.00
+                    },
+                    'after': {
+                        'value': 0.00,
+                        'pct': 0.00
+                    }
+                }
+            }
+        }
+
     stats = {'total': total,
              'budgets': {}}
         
-    unknown_value_before = total
-    unknown_value_after = total
+    unknown_value_before = total or 0
+    unknown_value_after = total or 0
 
     for b in before:
         if b.code not in stats['budgets']:
@@ -140,183 +159,16 @@ def get_colour(node, ccolours):
         if node in ccolours: return ccolours[node]
     return node
 
-def generate_sankey_data(country_code, budget_type):
-    sql_reporting_org_cc = """
-    SELECT sum(atransaction.value*sector.percentage/100) AS sum_value,
-    activity.reporting_org_ref, commoncode.category_EN
-    FROM atransaction
-    JOIN activity ON
-        activity.iati_identifier=atransaction.activity_iati_identifier
-    JOIN sector ON
-        activity.iati_identifier = sector.activity_iati_identifier
-    LEFT JOIN dacsector ON sector.code = dacsector.code
-    LEFT JOIN commoncode ON dacsector.cc_id = commoncode.id
-    LEFT JOIN ccbudgetcode ON commoncode.id = ccbudgetcode.cc_id
-    LEFT JOIN budgetcode ON ccbudgetcode.budgetcode_id = budgetcode.id
-    WHERE sector.deleted = 0
-    AND activity.recipient_country_code="%s"
-    AND activity.aid_type_code IN ('C01', 'D01', 'D02')
-    AND activity.status_code IN (2, 3)
-    AND atransaction.transaction_type_code="C"
-    AND budgetcode.country_code="%s"
-    AND budgetcode.budgettype_code = "%s"
-    GROUP BY commoncode.category_EN, activity.reporting_org_ref
-    ;"""
-            
-    sql_cc_budgetcode = """
-    SELECT sum(atransaction.value*sector.percentage/100) AS sum_value,
-    budgetcode.code, budgetcode.name, commoncode.category_EN
-    FROM atransaction
-    JOIN activity ON
-        activity.iati_identifier=atransaction.activity_iati_identifier
-    JOIN sector ON
-        activity.iati_identifier = sector.activity_iati_identifier
-    LEFT JOIN dacsector ON sector.code = dacsector.code
-    LEFT JOIN commoncode ON dacsector.cc_id = commoncode.id
-    LEFT JOIN ccbudgetcode ON commoncode.id = ccbudgetcode.cc_id
-    LEFT JOIN budgetcode ON ccbudgetcode.budgetcode_id = budgetcode.id
-    WHERE sector.deleted = 0
-    AND activity.recipient_country_code="%s"
-    AND activity.aid_type_code IN ('C01', 'D01', 'D02')
-    AND activity.status_code IN (2, 3)
-    AND atransaction.transaction_type_code="C"
-    AND budgetcode.country_code="%s"
-    AND budgetcode.budgettype_code = "%s"
-    GROUP BY budgetcode.code, commoncode.category_EN
-    ;"""
+def country_project_stats(country_code):
 
-    reporting_org_cc = db.engine.execute(sql_reporting_org_cc % (
-            country_code, country_code, budget_type)
-            )
-    cc_budgetcode = db.engine.execute(sql_cc_budgetcode % (
-            country_code, country_code, budget_type)
-            )
-    
-    node_data = {
-        "known": {},
-        "num_known": 0
-    }
-    links = []
-    
-    def notNull(value, typeof):
-        if (value == None or value == "") and typeof == "category":
-            return "Unmapped"
-        if (value == None or value == "") and typeof == "budgetcode":
-            return "Unknown budget code"
-        return value
-    
-    def get_node_code(node_name, node_data):
-        node_name = node_name
-        if node_name not in node_data['known']:
-            node_data['known'][node_name] = node_data['num_known']
-            node_data['num_known'] += 1
-        return node_data['known'][node_name]
-    
-    for rc in reporting_org_cc:
-        links.append({
-            'source': get_node_code(rc.reporting_org_ref, node_data),
-            'target': get_node_code(
-                    notNull(rc.category_EN, "category"), node_data
-                    ),
-            'value': rc.sum_value
-        })
-    
-    for cb in cc_budgetcode:
-        links.append({
-            'source': get_node_code(
-                notNull(cb.category_EN, "category"), node_data
-                ),
-            'target': get_node_code(
-                notNull(cb.name, "budgetcode"), node_data
-                ),
-            'value': cb.sum_value
-        })
-        
-    ccolours = country_colours.colours(country_code)
-
-    nodes = [{'name': node,
-              'colour': get_colour(node, ccolours)} for node in
-               sorted(node_data['known'], key=node_data['known'].get)]
-
-    from operator import itemgetter
-    links = sorted(links, key=itemgetter('value'))
-
-    out = {'nodes': nodes, 'links': links}
-
-    reporting_org_cc.close()
-    cc_budgetcode.close()
-
-    return out
-
-def country_project_stats(country_code, aid_types=["C01", "D01", "D02"], 
-                                        activity_statuses=[2,3]):
-
-    original_p = abprojects.projects(country_code)
-
-    total_projects = len(original_p)
-
-    def aid_type_filter(the_project):
-        return (the_project.aid_type_code in aid_types) and (
-                    the_project.status_code in activity_statuses)
-
-    p = filter(aid_type_filter, original_p)
-
-    filtered_projects = len(p)
-
+    projects = abprojects.projects(country_code)
+    total_projects = len(projects)
     total_value = sum(map(lambda project:
-         util.none_is_zero(project.total_commitments), original_p))
-    total_filtered_value = sum(map(lambda project:
-         util.none_is_zero(project.total_commitments), p))
-    total_mappable_before = sum(map(lambda project:
-         util.none_is_zero(project.pct_mappable_before)/100 *
-                util.none_is_zero(project.total_commitments), p))
-    total_mappable_after = sum(map(lambda project:
-         util.none_is_zero(project.pct_mappable_after)/100 *
-                util.none_is_zero(project.total_commitments), p))
-
-    total_mappable_before_admin = sum(map(lambda project:
-         util.none_is_zero(project.pct_mappable_before_admin)/100 *
-                util.none_is_zero(project.total_commitments), p))
-    total_mappable_after_admin = sum(map(lambda project:
-         util.none_is_zero(project.pct_mappable_after_admin)/100 *
-                util.none_is_zero(project.total_commitments), p))
-    admin = bool(total_mappable_after_admin > 0)
-
-    total_capital_before = 0.00
-    total_capital_after = sum(map(lambda project:
-        project.capital_exp * util.none_is_zero(project.total_commitments),
-        filter(util.filter_none_out, p)))
-    total_na_after = sum(map(lambda project: 1 * 
-        util.none_is_zero(project.total_commitments),
-        filter(util.filter_none_in, p)))
-    total_current_after = (
-        total_filtered_value -
-        total_capital_after -
-        total_na_after
-        )
+         util.none_is_zero(project.total_commitments), projects))
     return {
         "total_value": "{:,}".format(total_value),
-        "total_filtered_value": "{:,}".format(total_filtered_value),
-        "total_mappable_before": total_mappable_before,
-        "total_mappable_before_pct": round(
-            total_mappable_before/total_filtered_value*100, 2),
-        "total_mappable_after": total_mappable_after,
-        "total_mappable_after_pct": round(
-            total_mappable_after/total_filtered_value*100, 2),
-        "total_capital_before_pct": round(
-            total_capital_before/total_filtered_value*100, 2),
-        "total_capital_after_pct": round(
-            total_capital_after/total_filtered_value*100, 2),
-        "total_current_after_pct": round(
-            total_current_after/total_filtered_value*100, 2),
         "total_projects": total_projects,
-        "filtered_projects": filtered_projects,
-        "admin": admin,
-        "total_mappable_before_pct_admin": round(
-            total_mappable_before_admin/total_filtered_value*100, 2),
-        "total_mappable_after_pct_admin": round(
-            total_mappable_after_admin/total_filtered_value*100, 2),
-           }
+       }
 
 def earliest_latest_disbursements(country_code):
     country = abprojects.country(country_code)
@@ -325,13 +177,13 @@ def earliest_latest_disbursements(country_code):
         AS fiscal_year
         FROM atransaction
         JOIN activity ON
-            activity.iati_identifier=atransaction.activity_iati_identifier
+            activity.id=atransaction.id
         WHERE activity.recipient_country_code = '%s'
-        AND atransaction.transaction_type_code = '%s'
+        AND atransaction.transaction_type_code IN('%s')
         GROUP BY fiscal_year
         """
     fydata_results = db.engine.execute(FYDATA_QUERY % (
-            country.fiscalyear_modifier, country_code, "D")
+            country.fiscalyear_modifier, country_code, "D','E")
             )
     fydata = map(lambda d: int(d[0]), fydata_results)
     max_fydata = max(fydata)
@@ -351,9 +203,9 @@ def sectors_stats():
     FROM dacsector
     LEFT JOIN sector ON sector.code = dacsector.code
     LEFT JOIN activity ON
-        activity.iati_identifier = sector.activity_iati_identifier
+        activity.id = sector.activity_id
     LEFT JOIN atransaction ON
-        activity.iati_identifier=atransaction.activity_iati_identifier
+        activity.id=atransaction.activity_id
     WHERE sector.deleted = 0
     AND atransaction.transaction_type_code='C'
     GROUP BY dacsector.code
