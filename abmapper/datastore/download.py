54c62d029f8922bbbbb972a3bad3dee6ea131187
Mark Brough <mark.brough@gmail.com>
2017-05-16 11:50:55 +0000

    Adjust download function a bit

--- abmapper/datastore/download.py
+++ abmapper/datastore/download.py
@@ -6,20 +6,30 @@ import os, datetime
 from abmapper import app, db
 from abmapper.query import models
 from abmapper.query import projects
+from abmapper.query import settings
+import parse
 
-URL = "http://iati-datastore.herokuapp.com/api/1/access/activity.xml?recipient-country=%s&reporting-org=%s&stream=True"
+URL = "http://datastore.iatistandard.org/api/1/access/activity.xml?recipient-country={}&reporting-org={}&stream=True"
 
-def download_data(country, reporting_org):
+def download_data(country_code, reporting_org, update_exchange_rates=False, save=False):
+    # Check if reporting org exists
+    reporting_org_obj = settings.reporting_org_by_code(reporting_org)
+    if reporting_org_obj:
+        reporting_org_id = reporting_org_obj.id
+    else:
+        reporting_org_id = settings.add_reporting_org(reporting_org)
     def write_data(doc):
         LOCAL_DIR=app.config["DATA_STORAGE_DIR"]
-        path = os.path.join(LOCAL_DIR, "iati_data_" + country + '.xml')
+        path = os.path.join(LOCAL_DIR, "iati_data_" + country_code + '.xml')
         iati_activities = etree.tostring(doc.find("iati-activities"))
         with file(path, 'w') as localFile:
             localFile.write(iati_activities)
 
-    the_url = URL % (country, reporting_org)
-    print the_url
-
+    the_url = URL.format(country_code, reporting_org)
+    print "Fetching data from {}".format(the_url)
     xml_data = urllib2.urlopen(the_url, timeout=60).read()
-    doc = etree.fromstring(xml_data)
-    write_data(doc)
+    doc = etree.fromstring(xml_data) #.xpath("//iati-activities")
+    if not save:
+        parse.parse_doc(country_code, reporting_org_id, doc, update_exchange_rates)
+    else:
+        write_data(doc)
