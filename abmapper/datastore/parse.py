1306d72ececdd0de56548e70104346d38b86761b
Mark Brough <mark.brough@gmail.com>
2017-05-16 11:52:24 +0000

    Also parse v2 data, use exchange rates module

--- abmapper/datastore/parse.py
+++ abmapper/datastore/parse.py
@@ -5,7 +5,14 @@ from abmapper.query import projects as abprojects
 from abmapper.query import sectors as absectors
 from abmapper.query import models
 from abmapper.lib import util
+import exchangerates
 import datetime
+
+class UnrecognisedVersionException(Exception):
+    pass
+
+class InvalidFormatException(Exception):
+    pass
 
 def getfirst(list):
     if len(list)>0:
@@ -20,7 +27,7 @@ def getDACSectors(sectors):
         percentage = getfirst(sector.xpath('@percentage'))
         if not checkDAC(code, DAC_codes): continue
         out.append({
-            'code': code,
+            'code': unicode(code),
             'percentage': percentage})
     return out, len(out)
 
@@ -31,24 +38,38 @@ def checkDAC(code, DAC_codes):
         return False
     return code in DAC_codes
 
+def get_recipient_countries(activity):
+    rcs = []
+    countries=activity.xpath(
+        'recipient-country')
+    for country in countries:
+        rc = models.RecipientCountries()
+        rc.recipient_country_code = unicode(country.get("code"))
+        rc.percentage = country.get("percentage", 100)
+        rcs.append(rc)
+    return rcs
+
 def get_sectors(activity):
     s = []
     sectors=activity.xpath(
-        'sector[@vocabulary="DAC"]|sector[not(@vocabulary)]')
+        'sector[@vocabulary="DAC"]|sector[not(@vocabulary)]|sector[@vocabulary="1"]')
     sectors, num_sectors=getDACSectors(sectors)
     for sector in sectors:
         ns = models.Sector()
-        ns.code = sector['code']
+        ns.code = unicode(sector['code'])
         ns.percentage = sector['percentage'] or 100
         s.append(ns)
     return s
 
-def parse_org(xml):
+def parse_org(xml, version):
     data = {
-        "ref": xml.get("ref", ""),
-        "name": getfirst(xml.xpath('text()')),
-        "type": xml.get("@type")
+        "ref": unicode(xml.get("ref", "")),
+        "name": unicode(getfirst(xml.xpath('text()'))),
+        "type": unicode(xml.get("@type"))
         }
+    #FIXME: Need to make this properly multilingual where organisations can be published in multiple names
+    if version == 2: 
+        data["name"] = unicode(getfirst(xml.xpath("narrative/text()")))
     if not data.get("ref") and not data.get("name"):
         return False
     check = models.Organisation.query.filter_by(
@@ -58,12 +79,27 @@ def parse_org(xml):
     if check: return check
     return models.Organisation.as_unique(db.session, **data)
 
-def get_orgs(activity, country_code):
+def get_o_role(value):
+    try:
+        types = {
+            "1": u"Funding",
+            "2": u"Accountable",
+            "3": u"Extending",
+            "4": u"Implementing"
+        }
+        return types[value]
+    except KeyError:
+        raise InvalidFormatException("""{} is not a valid participating 
+        organisation role for this version of the IATI Standard""")
+
+def get_orgs(activity, country_code, version):
     ret = []
     seen = set()
     for ele in activity.xpath("./participating-org"):
-        role = ele.get("role", "Funding")
-        organisation = parse_org(ele)
+        role = unicode(ele.get("role", {1: u"Funding", 2: u"1"}[version]))
+        if version == 2:
+            role = get_o_role(role)
+        organisation = parse_org(ele, version)
         if organisation and not (role, organisation.name) in seen:
             seen.add((role, organisation.name))
             ret.append(models.Participation(role=role,
@@ -74,22 +110,44 @@ def get_orgs(activity, country_code):
 def get_currency(activity, transaction):
     ac = activity.get("default-currency")
     if ac and ac != "":
-        return ac
+        return unicode(ac)
     tc = transaction.xpath("value/@currency")
     if tc and tc != "":
-        return tc
-    return "USD"
+        return unicode(tc)
+    return u"USD"
 
-def get_transactions(activity):
+def get_t_type(value):
+    try:
+        types = {
+            "1": u"IF",
+            "2": u"C",
+            "3": u"D",
+            "4": u"E",
+            "5": u"IR",
+            "6": u"LR",
+            "7": u"R",
+            "8": u"QP",
+            "9": u"Q3",
+            "10": u"CG"
+        }
+        return types[value]
+    except KeyError:
+        raise InvalidFormatException("""{} is not a valid transaction type 
+        for this version of the IATI Standard""")
+
+def get_transactions(activity, iati_identifier, version, exchange_rates):
     ret = []
     for ele in activity.xpath("./transaction"):
         currency = get_currency(activity, ele)
-        exchange_rate = util.exchange_rates()[currency]
-
-        t_type = getfirst(ele.xpath("transaction-type/@code"))
+        t_type = unicode(getfirst(ele.xpath("transaction-type/@code")))
+        if version == 2:
+            t_type = get_t_type(t_type)
         t_date = getfirst(ele.xpath("transaction-date/@iso-date"))
         t_value = getfirst(ele.xpath("value/text()"))
         t_value_date = getfirst(ele.xpath("value/@value-date"))
+        
+        exchange_rate = exchange_rates.closest_rate(
+            currency, util.make_date_from_iso(t_value_date))["conversion_rate"]
         
         #FIXME for WB data which puts t_date in the wrong place
         if activity.xpath("reporting-org/@ref='US'"):
@@ -108,35 +166,36 @@ def get_transactions(activity):
             print "t_value_date is", t_value_date
             continue
 
-        iati_identifier = getfirst(activity.xpath("iati-identifier/text()"))
-
         tr = models.Transaction()
-        tr.activity_iati_identifier = iati_identifier
         tr.value = (float(t_value) * exchange_rate)
         tr.value_date = datetime.datetime.strptime(
                 t_value_date, "%Y-%m-%d" )
-        tr.value_currency = ""
+        tr.value_currency = u"USD"
         tr.transaction_date = datetime.datetime.strptime(
                 t_date, "%Y-%m-%d" )
         tr.transaction_type_code = t_type
         ret.append(tr)
     return ret 
 
-def get_titles(activity):
+def get_titles(activity, version):
     ret = []
-    for ele in activity.xpath("./title"):
+    loop = {1: activity.xpath("./title"),
+            2: activity.xpath("./title/narrative")}[version]
+    for ele in loop:
         title = models.Title()
-        title.text = ele.text
-        title.lang = getfirst(ele.xpath('@xml:lang'))
+        title.text = unicode(ele.text)
+        title.lang = unicode(getfirst(ele.xpath('@xml:lang')))
         ret.append(title)
     return ret
 
-def get_descriptions(activity):
+def get_descriptions(activity, version):
     ret = []
-    for ele in activity.xpath("./description"):
+    loop = {1: activity.xpath("./description"),
+            2: activity.xpath("./description/narrative")}[version]
+    for ele in loop:
         description = models.Description()
-        description.text = ele.text
-        description.lang = getfirst(ele.xpath('@xml:lang'))
+        description.text = unicode(ele.text)
+        description.lang = unicode(getfirst(ele.xpath('@xml:lang')))
         ret.append(description)
     return ret
 
@@ -155,34 +214,53 @@ def null_to_default(value, default):
         return default
     return value
 
-def write_activity(activity, country_code):
+def write_activity(activity, country_code, reporting_org_id, version, exchange_rates):
     a = models.Activity()
-    a.iati_identifier = getfirst(activity.xpath('iati-identifier/text()'))
-    a.all_titles = get_titles(activity)
-    a.all_descriptions = get_descriptions(activity)
+    a.iati_identifier = unicode(getfirst(activity.xpath('iati-identifier/text()')))
+    a.reporting_org_id = reporting_org_id
+    a.all_titles = get_titles(activity, version)
+    a.recipient_country_code = unicode(country_code)
+    a.recipient_countries = get_recipient_countries(activity)
+    a.all_descriptions = get_descriptions(activity, version)
     a.sectors = get_sectors(activity)
-    a.participating_orgs = get_orgs(activity, country_code)
-    a.reporting_org_ref = getfirst(activity.xpath('reporting-org/@ref'))
-    a.transactions = get_transactions(activity)
-    a.status_code = null_to_default(
+    a.participating_orgs = get_orgs(activity, country_code, version)
+    a.transactions = get_transactions(activity, a.iati_identifier, 
+                                      version, exchange_rates)
+    a.status_code = unicode(null_to_default(
         getfirst(activity.xpath('activity-status/@code')),
         "2"
-        )
-    a.aid_type_code = null_to_default(
+        ))
+    a.aid_type_code = unicode(null_to_default(
 getfirst(activity.xpath('default-aid-type/@code|transaction/aid-type/@code')),
         "C01"
-        )
-    a.date_start_planned = get_date(activity, 'start-planned')
-    a.date_end_planned = get_date(activity, 'end-planned')
-    a.date_start_actual = get_date(activity, 'start-actual')
-    a.date_end_actual = get_date(activity, 'end-actual')
-    a.recipient_country_code = country_code
+        ))
+    a.date_start_planned = get_date(activity, {2: 'start-planned', 1: "1"}[version])
+    a.date_end_planned = get_date(activity, {2: 'end-planned', 1: "3"}[version])
+    a.date_start_actual = get_date(activity, {2: 'start-actual', 1: "2"}[version])
+    a.date_end_actual = get_date(activity, {2: 'end-actual', 1: "4"}[version])
     db.session.add(a)
     db.session.commit()
 
+def get_version(activity):
+    ns = activity.nsmap["iati-extra"]
+    version = activity.get("{%s}version" % ns, "1.05")
+    if version in ("2.01", "2.02"):
+        return 2
+    elif version in ("1.01", "1.05"):
+        return 1
+    raise UnrecognisedVersionException("""The IATI-XML version {} was not 
+    recognised.""".format(version))
+
+def parse_doc(country_code, reporting_org_id, doc, update_exchange_rates=True, sample=False):
+    exchange_rates = exchangerates.CurrencyConverter(update=update_exchange_rates)
+    activities = doc.xpath('//iati-activity')
+    print("Parsing {} activities".format(len(activities)))
+    for i, activity in enumerate(activities):
+        version = get_version(activity)
+        write_activity(activity, unicode(country_code), reporting_org_id,
+                       version, exchange_rates)
+        if sample and i>50: break
+
 def parse_file(country_code, filename, sample=False):
     doc=etree.parse(filename)
-    activities = doc.xpath('//iati-activity')
-    for i, activity in enumerate(activities):
-        write_activity(activity, country_code)
-        if sample and i>50: break
+    parse_doc(doc)
