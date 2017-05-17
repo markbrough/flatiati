075847578e4933cf0445aefb3fa4cf5e9c8ee2e5
Mark Brough <mark.brough@gmail.com>
2017-05-17 10:17:48 +0000

    Rewrite setup to generate and use local cached codelists

--- abmapper/query/setup.py
+++ abmapper/query/setup.py
@@ -7,10 +7,24 @@ import urllib2
 import json
 from flask import abort
 
-CODELISTS_API = "http://iatistandard.org/codelists/downloads/clv2/json/%s/%s.json"
+CODELIST_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../lib/codelists/{}.csv')
 
 def trash():
     db.drop_all()
+
+def update_codelists():
+    codelists = {
+        "ActivityStatus": "http://iatistandard.org/202/codelists/downloads/clv2/csv/en/ActivityStatus.csv",
+        "AidType": "https://andylolz.github.io/dac-crs-codes/data/aid_type.csv",
+        "Country": "https://github.com/datasets/country-codes/raw/master/data/country-codes.csv",
+        "Sector": "https://andylolz.github.io/dac-crs-codes/data/sector.csv",
+        "SectorCategory": "https://andylolz.github.io/dac-crs-codes/data/sector_category.csv"
+    }
+    for code, url in codelists.items():
+        sourcedata = urllib2.urlopen(url, timeout=60).read()
+        path = CODELIST_PATH.format(code)
+        with file(path, 'w') as localFile:
+            localFile.write(sourcedata)
 
 def setup():
     db.create_all()
@@ -20,99 +34,69 @@ def setup():
     import_recipient_countries()
     import_budget_types()
 
-def import_common_code():
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../lib/CC_EN_FR.csv'), 'r') as csvfile:
-        ccreader = unicodecsv.DictReader(csvfile)
-        for row in ccreader:
-            cc = models.CommonCode()
-            cc.id = row["CC code"]
-            cc.category_EN = row["Category of Government"]
-            cc.sector_EN = row["Sector"]
-            cc.function_EN = row["Function"]    
-            cc.category_FR = row["Categorie de gouvernement"]
-            cc.sector_FR = row["Secteur"]
-            cc.function_FR = row["Fonction"]
-            db.session.add(cc)
-        db.session.commit()
-
 def import_sectors():
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                     '../lib/crs_cc_EN.csv'), 'r') as csvfile:
-        crsccreader = unicodecsv.DictReader(csvfile)
-        for row in crsccreader:
-            crscc = models.DACSector()
-            crscc.code = row["CRS_code"]
-            crscc.dac_sector_code = row["DAC_sector_code"]
-            crscc.dac_sector_name_EN = row["DAC_sector_name"]
-            crscc.dac_five_code = row["DAC_5_code"]
-            crscc.dac_five_name_EN = row["DAC_5_name"]
-            crscc.description_EN = row["DESCRIPTION"]
-            crscc.notes_EN = row["notes"]
-            crscc.parent_code = row["parent_code"]
-            db.session.add(crscc)
-        db.session.commit()
-    # Get FR sector names
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../lib/crs_cc_FR.csv'), 'r') as csvfile:
-        crsccreader = unicodecsv.DictReader(csvfile)
-        for row in crsccreader:
-            crscc = models.DACSector.query.filter_by(
-                code=row["CRS_code"]).first()
-            crscc.dac_sector_name_FR = row["DAC_sector_name"]
-            crscc.dac_five_name_FR = row["DAC_5_name"]
-            crscc.description_FR = row["DESCRIPTION"]
-            crscc.notes_FR = row["notes"]
-            db.session.add(crscc)
+    SectorCategories = {}
+    with open(CODELIST_PATH.format("SectorCategory"), 'r') as scfile:
+        for row in unicodecsv.DictReader(scfile):
+            SectorCategories[row["code"]] = row
+
+    with open(CODELIST_PATH.format("Sector"), 'r') as sfile:
+        for row in unicodecsv.DictReader(sfile):
+            dacsector = models.DACSector()
+            if row["voluntary_code"] != "": 
+                dacsector.code = row["voluntary_code"]
+            else:
+                dacsector.code = row["code"]
+            #dacsector.dac_sector_code = row["DAC_sector_code"] #NO
+            #dacsector.dac_sector_name_EN = row["DAC_sector_name"] #NO
+            #dacsector.dac_sector_name_FR = row["DAC_sector_name"] #NO
+            dacsector.dac_five_code = SectorCategories[row["category"]]["code"]
+            dacsector.dac_five_name_EN = SectorCategories[row["category"]]["name_en"]
+            dacsector.dac_five_name_FR = SectorCategories[row["category"]]["name_fr"]
+            dacsector.description_EN = row["name_en"]
+            dacsector.description_FR = row["name_fr"]
+            dacsector.notes_EN = row["description_en"]
+            dacsector.notes_FR = row["description_fr"]
+            db.session.add(dacsector)
         db.session.commit()
 
 def import_activity_statuses():
-    aid_type_url = CODELISTS_API % ("en", 'ActivityStatus')
-    sourcedata = urllib2.urlopen(aid_type_url, timeout=60).read()
-    data = json.loads(sourcedata)['data']
-    for code in data:
-        activitystatus = models.ActivityStatus()
-        activitystatus.code = code["code"]
-        activitystatus.text_EN = code["name"]
-        db.session.add(activitystatus)
-    db.session.commit()
-    aid_type_url = CODELISTS_API % ("fr", 'ActivityStatus')
-    sourcedata = urllib2.urlopen(aid_type_url, timeout=60).read()
-    data = json.loads(sourcedata)['data']
-    for code in data:
-        activitystatus = models.ActivityStatus.query.filter_by(
-            code = code["code"]
-        ).first()
-        activitystatus.text_FR = code["name"]
-        db.session.add(activitystatus)
-    db.session.commit()
+    with open(CODELIST_PATH.format("ActivityStatus"), 'r') as csvfile:
+        statusreader = unicodecsv.DictReader(csvfile)
+        for row in statusreader:
+            activitystatus = models.ActivityStatus()
+            activitystatus.code = row["code"]
+            activitystatus.text_EN = row["name"]
+            activitystatus.text_FR = row["name"]
+            db.session.add(activitystatus)
+        db.session.commit()
 
-def import_aid_types(): 
-    filename="aid_type_EN_FR.csv"
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../lib/'+filename), 'r') as csvfile:
+def import_aid_types():
+    with open(CODELIST_PATH.format("AidType"), 'r') as csvfile:
         aidtypereader = unicodecsv.DictReader(csvfile)
         for row in aidtypereader:
             nc = models.AidType()
-            nc.code = row["Code"]
-            nc.text_EN = row["EN_Description"]
-            nc.text_FR = row["FR_Description"]
+            nc.code = row["code"]
+            nc.text_EN = row["name_en"]
+            nc.text_FR = row["name_fr"]
             db.session.add(nc)
     db.session.commit()
 
 def import_recipient_countries():
-    # Only EN available via IATI API
-    aid_type_url = CODELISTS_API % ("en", 'Country')
-    sourcedata = urllib2.urlopen(aid_type_url, timeout=60).read()
-    data = json.loads(sourcedata)['data']
-    for code in data:
-        rc = models.RecipientCountry()
-        rc.code = code["code"]
-        rc.text_EN = code["name"]
-        rc.text_FR = code["name"]
-        db.session.add(rc)
+    with open(CODELIST_PATH.format("Country"), 'r') as csvfile:
+        countryreader = unicodecsv.DictReader(csvfile)
+        for row in countryreader:
+            if (row["name"] == "") or (row["ISO3166-1-Alpha-2"] == ""): continue
+            rc = models.RecipientCountry()
+            rc.code = row["ISO3166-1-Alpha-2"]
+            rc.text_EN = row["official_name_en"]
+            rc.text_FR = row["official_name_fr"]
+            db.session.add(rc)
     db.session.commit()
 
 def import_budget_types():
     filename="budget_type_EN_FR.csv"
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../lib/'+filename), 'r') as csvfile:
+    with open(CODELIST_PATH.format("budget_type_EN_FR"), 'r') as csvfile:
         budgettypereader = unicodecsv.DictReader(csvfile)
         for row in budgettypereader:
             nc = models.BudgetType()
