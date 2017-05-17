70b6417c0ce55e398e00c171d6be6d338b3862c4
Mark Brough <mark.brough@gmail.com>
2017-05-17 15:01:45 +0000

    Update downloader to check for other related activities and to use flattener

--- abmapper/datastore/download.py
+++ abmapper/datastore/download.py
@@ -10,6 +10,7 @@ from abmapper.query import settings
 import parse
 
 URL = "http://datastore.iatistandard.org/api/1/access/activity.xml?recipient-country={}&reporting-org={}&stream=True"
+IDENTIFIER_URL = "http://datastore.iatistandard.org/api/1/access/activity.xml?iati-identifier={}&stream=True"
 
 def download_data(country_code, reporting_org, update_exchange_rates=False, save=False):
     # Check if reporting org exists
@@ -25,10 +26,27 @@ def download_data(country_code, reporting_org, update_exchange_rates=False, save
         with file(path, 'w') as localFile:
             localFile.write(iati_activities)
 
-    the_url = URL.format(country_code, reporting_org)
-    print "Fetching data from {}".format(the_url)
-    xml_data = urllib2.urlopen(the_url, timeout=60).read()
-    doc = etree.fromstring(xml_data) #.xpath("//iati-activities")
+    def get_doc(the_url):
+        print "Fetching data from {}".format(the_url)
+        xml_data = urllib2.urlopen(the_url, timeout=60).read()
+        return etree.fromstring(xml_data)
+
+    doc = get_doc(URL.format(country_code, reporting_org))
+
+    # Check if complete for hierarchies
+    iati_identifiers = doc.xpath("//iati-identifier/text()")
+    related_activity_identifiers = doc.xpath("//related-activity[@type='1' or @type='2']/@ref")
+    def filter_related_doesnt_exist(related_identifier):
+        return related_identifier not in iati_identifiers
+    unfound_identifiers = filter(filter_related_doesnt_exist, related_activity_identifiers)
+
+    if len(unfound_identifiers)>0:
+        ufdoc = get_doc(IDENTIFIER_URL.format("|".join(unfound_identifiers)))
+        ufdoc_activities = ufdoc.xpath("//iati-activity")
+        doc_iati_activities = doc.find("iati-activities")
+        for ufdoc_activity in ufdoc_activities:
+            doc_iati_activities.append(ufdoc_activity)
+
     if not save:
         parse.parse_doc(country_code, reporting_org_id, doc, update_exchange_rates)
     else:
