2fe8e5f30b91dd60ddbe859c0ebb9aee8fd54711
Mark Brough <mark.brough@gmail.com>
2018-01-25 19:34:11 +0000

    Adjust name of app, add freezer

--- manage.py
+++ manage.py
@@ -1,19 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#  IATI Data Quality, tools for Data QA on IATI-formatted  publications
-#  by Mark Brough, Martin Keegan, Ben Webb and Jennifer Smith
+#  Flat IATI, generate spreadsheets from IATI data
 #
 #  Copyright (C) 2013  Publish What You Fund
+#  Copyright (C) 2017  Mark Brough, Overseas Development Institute
 #
 #  This programme is free software; you may redistribute and/or modify
 #  it under the terms of the GNU Affero General Public License v3.0
 
-from flask.ext.script import Manager
+from flask.ext.script import Manager, Command
+from flask_frozen import Freezer
 import abmapper
+from abmapper import app
+
+app.config['FREEZER_IGNORE_ENDPOINTS'] = ['admin', 'api_reporting_orgs']
+
+def no_argument_rules_urls_with_ignore():
+    """URL generator for URL rules that take no arguments."""
+    ignored = app.config.get('FREEZER_IGNORE_ENDPOINTS', [])
+    for rule in app.url_map.iter_rules():
+        if rule.endpoint not in ignored and not rule.arguments and 'GET' in rule.methods:
+            yield rule.endpoint, {}
+
+freezer = Freezer(app=app, with_no_argument_rules=False)
+freezer.register_generator(no_argument_rules_urls_with_ignore)
+
+class Freeze(Command):
+    "Freezes site and spreadsheets"
+    
+    def run(self):
+        app.config["FREEZE"] = True
+
+        print ""
+        print "Freezing site"
+        freezer.freeze()
 
 def run():
-    manager = Manager(abmapper.app)
+    manager = Manager(app)
+    manager.add_command('freeze', Freeze())
     manager.run()
 
 if __name__ == "__main__":
