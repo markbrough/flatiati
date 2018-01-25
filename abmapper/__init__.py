2e78010aa5396fd1d1263ea5b88be3b318b89cda
Mark Brough <mark.brough@gmail.com>
2018-01-25 19:17:49 +0000

    Remove redundant code

--- abmapper/__init__.py
+++ abmapper/__init__.py
@@ -34,23 +34,8 @@ def get_locale():
     # header the browser transmits. The best match wins.
     return request.accept_languages.best_match(LANGUAGES)
 
-def is_safe_url(target):
-    ref_url = urlparse(request.host_url)
-    test_url = urlparse(urljoin(request.host_url, target))
-    return test_url.scheme in ('http', 'https') and \
-           ref_url.netloc == test_url.netloc
-
-def get_redirect_target():
-    if request.referrer and is_safe_url(request.referrer):
-        return request.referrer
-    return url_for("dashboard")
-
-@app.route("/lang/<lang>/")
 def set_lang(lang):
-    print LANGUAGES
     if lang in LANGUAGES:
         session['lang'] = lang
-        return redirect(get_redirect_target())
     else:
         flash("Unrecognised language", "error")
-        return redirect(get_redirect_target())
