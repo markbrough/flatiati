5b786d71c79c1e0da5e2c8c139959e64c8240609
Mark Brough <mark.brough@gmail.com>
2018-01-30 12:57:19 +0000

    Update freeze script

--- getfreeze.sh
+++ getfreeze.sh
@@ -5,12 +5,14 @@ git init .
 git remote add origin git@github.com:markbrough/flatiati.git
 git pull origin gh-pages
 git checkout gh-pages
-rm -rf *
+rm -rf en fr index.html static
 cd ../../
 ./abtool.py --update-exchange-rates
 ./abtool.py --update-all
 ./manage.py freeze
 cd abmapper/build
+git init .
+echo "spreadsheets.aidonbudget.org" > CNAME
 git add .
 git commit -a -m "Update"
 git push origin gh-pages
