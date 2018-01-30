2cc388612f2af27790a068b77f31b83960a32a39
Mark Brough <mark.brough@gmail.com>
2018-01-30 11:54:20 +0000

    Also push to Github

--- getfreeze.sh
+++ getfreeze.sh
@@ -1,3 +1,18 @@
+source ./pyenv/bin/activate
+mkdir abmapper/build
+cd abmapper/build
+git init .
+git remote add origin git@github.com:markbrough/flatiati.git
+git pull origin gh-pages
+git checkout gh-pages
+rm -rf *
+cd ../../
 ./abtool.py --update-exchange-rates
 ./abtool.py --update-all
 ./manage.py freeze
+cd abmapper/build
+git add .
+git commit -a -m "Update"
+git push origin gh-pages
+rm -rf *
+rm -rf .git
